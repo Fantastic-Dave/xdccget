bb79b6832ac5381093d158e28155356fa5d3f48b
Julian Cordes <julian.cordes@gmail.com>
2016-04-04 15:18:02 +0200

    removed dependency from header

--- xdccget.h
+++ xdccget.h
@@ -2,9 +2,8 @@
 #define XDCCGET_H
 
 #include "helper.h"
-#include <stdnoreturn.h>
 
 struct xdccGetConfig *getCfg();
-void /*__attribute__ ((noreturn))*/ exitPgm(int retCode);
+void exitPgm(int retCode);
 
 #endif
